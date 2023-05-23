8b98e20bef45fe9fb8281565f3ab16cfe8ef7ca5
aidan <aidanguarniere@gmail.com>
2023-05-23 12:25:58 -0700

    basic service level auth integration and prep for heroku deployment

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const openApiValidator = require('express-openapi-validator');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const openApiValidator = require("express-openapi-validator");
+const path = require("path");
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const SERVICE_AUTH_KEY = process.env.SERVICE_AUTH_KEY;
+
+function assertAuthHeader(req, res, next) {
+  if (req.headers.authorization !== `Bearer ${SERVICE_AUTH_KEY}`) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+  next();
+}
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
-app.use('/openapi.json', express.static(path.join(__dirname, 'openapi.json')));
-app.use('/logo.png', express.static(path.join(__dirname, 'logo.png')));
+app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
+app.use("/openapi.json", express.static(path.join(__dirname, "openapi.json")));
+app.use("/logo.png", express.static(path.join(__dirname, "logo.png")));
 
 const todos = {};
 
-app.get('/todos/:username', (req, res) => {
+app.get("/todos/:username", assertAuthHeader, (req, res) => {
   const { username } = req.params;
   res.json({ todos: todos[username] || [] });
 });
 
-app.post('/todos/:username', (req, res) => {
+app.post("/todos/:username", assertAuthHeader, (req, res) => {
   const { username } = req.params;
   const { todo } = req.body;
   if (!todos[username]) {
@@ -29,7 +39,7 @@ app.post('/todos/:username', (req, res) => {
   res.json({ todos: todos[username] });
 });
 
-app.delete('/todos/:username', (req, res) => {
+app.delete("/todos/:username", assertAuthHeader, (req, res) => {
   const { username } = req.params;
   const { todo_idx } = req.body;
   if (todos[username] && todos[username][todo_idx]) {
@@ -38,11 +48,13 @@ app.delete('/todos/:username', (req, res) => {
   res.json({ todos: todos[username] || [] });
 });
 
-app.use(openApiValidator.middleware({
-  apiSpec: path.join(__dirname, 'openapi.json'),
-  validateRequests: true,
-  validateResponses: true
-}));
+app.use(
+  openApiValidator.middleware({
+    apiSpec: path.join(__dirname, "openapi.json"),
+    validateRequests: true,
+    validateResponses: true,
+  })
+);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
@@ -51,6 +63,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
